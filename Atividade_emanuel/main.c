e44153bfc6128e18ee0675b33d10d1bac06c085c
jeffitequila <43590568+jeffitequila@users.noreply.github.com>
2018-09-25 22:28:36 -0300

    Primeira tentativa

---
+++ Atividade_emanuel/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <conio.h>
+int main()
+    //int voto;
+{
+    int canditado1 , canditado2 , canditado3 , canditado4 , brancos, nulos;
+
+    printf("ELEICAO 2018\n");
+        printf("ESCOLHA SEU CANDIDATO................\n");
+        printf("1.LEVI FIDELIS.......................\n");
+        printf("2.ILDON..............................\n");
+        printf("3.MADEIRA............................\n");
+        printf("4.CANDIDATO QUE NINGUEM CONHECE......\n");
+        printf("5.BRANCOS............................\n");
+        printf("6.NULOS..............................\n");
+
+ //CHAMANDO A FUNÇÃO...
+        void ler_voto();
+            ler_voto;
+         int escolher_voto(int voto);
+            escolher_voto;
+    return 0;
+}
+    int voto;
+
+    void ler_voto(){
+    scanf("%i",&voto);
+
+
+    }a
+//FUNÇÃO DE ARMAZENAR OS VOTOS
+    int escolher_voto(int voto){
+        switch (voto)
+        case '1':
+            candidato1 = canditato1 + 1
+        case '2':
+            candidato2 = canditado2 + 1
+        case '3':
+            candidato3 = canditado3 + 1
+        case '4':
+            candidato4 = canditado4 + 1
+        case '5':
+            branco = branco + 1
+        case '6':
+            nulo = nulo + 1
+    return 0;
+    };
+
